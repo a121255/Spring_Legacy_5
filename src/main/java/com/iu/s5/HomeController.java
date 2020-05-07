d4cbbc374f6c94fa256d2e448b6cba867210e59e
a121255 <appleybc@naver.com>
2020-05-07 12:19:01 +0900

    xml

--- src/main/java/com/iu/s5/HomeController.java
+++ src/main/java/com/iu/s5/HomeController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,33 +28,25 @@ public class HomeController {
 
 //	private String user;
 	
+	@Autowired
 	private Transfer transfer;
+	
+	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 //		System.out.println(user);
 		
 		
+
+		transfer.bus(1000);
+
 		
-		
-		Card card = new Card();
-		
-		card.cardCheck();
-		transfer.bus();
-		card.cardCheck();
-		
-		
-		card.cardCheck();
-		transfer.subway();
-		card.cardCheck();
-		
-		 
-		
-		
-		
-		
-		
-		
+
+		transfer.subway(2000, "2");
+
+		transfer.taxi();
+
 		return "index";
 	}
 	
--- src/main/java/com/iu/s5/transfer/Card.java
+++ src/main/java/com/iu/s5/transfer/Card.java
@@ -1,7 +1,33 @@
 package com.iu.s5.transfer;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Card {
-	public void cardCheck() {
-		System.out.println("카드 찍기~~");
+	
+	public void cardAfter() {
+		System.out.println("===========================");
+		System.out.println("카드 결제");
+	}
+	
+	
+	
+	
+	
+	public Object cardCheck(ProceedingJoinPoint join) throws Throwable {
+		System.err.println("-----------------------------");
+		System.out.println("카드 check in");
+		Object [] ar = join.getArgs();
+		
+		for(Object o : ar) {
+			System.out.println(o);
+		}
+		
+		Object obj = join.proceed();
+		
+		
+		
+		System.out.println("카드 check out");
+		
+		return obj;
 	}
 }
